7949b3a044aa835ec39a5f9f3442e56b827fbfdb
Maciek Chmura <chmura.m@gmail.com>
2018-11-21 22:23:02 +0100

    task2 fixes
    adding HtmlWebpackPlugin
    adding file-loader
    webpack confing with ENV variables

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  const modeOption = env.dev || !env.prod;
+
+  return {
+    mode: modeOption ? 'development' : 'production',
+    entry: './src/index.js',
+    devtool: modeOption ? 'source-map' : 'none',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            outputPath: 'images/',
+            publicPath: 'assets/'
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'React Mentoring'
+      })
+    ]
+  };
+};
