cdbf50a9b26179d3143a1eac9497d238f97ec3a6
Maciek Chmura <chmura.m@gmail.com>
2018-11-22 13:27:53 +0100

    webpack output publicPath change

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ module.exports = env => {
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      filename: './bundle.js'
+      publicPath: './',
+      filename: 'bundle.js'
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
